80ac6fe0c42134a7ba31290943bfba6987483f4c
patrickmao93 <patrickmao93@gmail.com>
2019-08-14 13:54:35 +0800

    basics done

---
+++ gen/js/dayofweek.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.DayOfWeek');
+
+/**
+ * @enum {number}
+ */
+proto.DayOfWeek = {
+  UNKNOWN: 0,
+  MONDAY: 1
+};
+
